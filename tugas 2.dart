c748564210720e654a35c0a057486ac3991a4414
Razdiablo <ramadhanibudiarto@gmail.com>
2023-10-20 15:17:23 +0700

    new

--- tugas 2.dart
+++ tugas 2.dart
@@ -16,15 +16,3 @@ List<String> fizzBuzz(int a, int b) =>
 // Kode 3
 List<int> twoNumber(List<int> l) =>
     List.generate(l.length - 1, (index) => l[index] + l[index + 1]);
-
-void main() {
-  // Menggunakan fungsi sequenceGenerator
-  print(sequenceGenerator(1, 10)); // Mencetak [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-  // Menggunakan fungsi fizzBuzz
-  print(fizzBuzz(1,
-      15)); // Mencetak ['1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz', 'Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']
-
-  // Menggunakan fungsi twoNumber
-  print(twoNumber([1, 2, 3, 4, 5])); // Mencetak [3, 5, 7, 9]
-}
